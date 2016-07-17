81d9b888574737c2b210ef6b253d4f312139cd1a
Yasin <ynooran@yahoo.com>
2016-07-17 06:55:44 +0430

    railway

--- main/OI_17_Railway.cpp
+++ main/OI_17_Railway.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include <queue>
 #define FOR(i,a,b) for(int i=a; i<b; i++)
 #define dbug(x) #x<<": "<<x<<' '
 using namespace std;
 int n;
-vector<int> a, stack[2], b, trs;
+vector<int> stack[2], b, trs;
+queue<int> a;
 bool ans(int inb)
 {
+	FOR(i,1,stack[0].size())
+		cout<<dbug(stack[0][i])<<" ";
+	cout<<endl;
+	FOR(i,1,stack[1].size())
+		cout<<dbug(stack[1][i])<<" ";
+	cout<<endl;
+	cout<<endl;
 	if(inb==n+1)
 		return true;
 	int bind = (stack[0].back() < stack[1].back() ? 0 : 1);
@@ -15,20 +23,42 @@ bool ans(int inb)
 	{
 		b.push_back(inb);
 		stack[bind].pop_back();
+	FOR(i,1,stack[0].size())
+		cout<<dbug(stack[0][i])<<" ";
+	cout<<endl;
+	FOR(i,1,stack[1].size())
+		cout<<dbug(stack[1][i])<<" ";
+	cout<<endl;
+	cout<<endl;
 		return ans(inb+1);
 	}
-	int x = a.back();
-	a.pop_back();
+	cout<<inb<<endl;
+	int x = a.front();
+	a.pop();
 	if(x<stack[bind].back())
 	{
 		trs.push_back(bind+1);
 		stack[bind].push_back(x);
+	FOR(i,1,stack[0].size())
+		cout<<dbug(stack[0][i])<<" ";
+	cout<<endl;
+	FOR(i,1,stack[1].size())
+		cout<<dbug(stack[1][i])<<" ";
+	cout<<endl;
+	cout<<endl;
 		return ans(inb);
 	}
 	else if(x<stack[bind^1].back())
 	{
 		trs.push_back((bind^1)+1);
 		stack[bind^1].push_back(x);
+	FOR(i,1,stack[0].size())
+		cout<<dbug(stack[0][i])<<" ";
+	cout<<endl;
+	FOR(i,1,stack[1].size())
+		cout<<dbug(stack[1][i])<<" ";
+	cout<<endl;
+	cout<<endl;
 		return ans(inb);
 	}
 	else
@@ -40,9 +70,8 @@ int main(){
 	{
 		int tmp;
 		cin>>tmp;
-		a.push_back(tmp);
+		a.push(tmp);
 	}
-	reverse(a.begin(),a.end());
 	stack[0].push_back(n+1);
 	stack[1].push_back(n+1);
 	if(ans(1))
