3099b7b9b1d535f214d35cf88ab0163eb2cfd62c
Yasin <ynooran@yahoo.com>
2016-09-02 13:29:50 +0430

    514C
    “hashing…”

--- CodeForces/514C.cpp
+++ CodeForces/514C.cpp
@@ -1,34 +1,36 @@
-#include <iostream>
-#include <algorithm>
-#include <set>
+#include <bits/stdc++.h>
+#define int long long
 #define FOR(i,a,b) for(int i=a; i<b; i++)
-#define dbug(x) #x<<": "<<x<<' '
 using namespace std;
-int n, m, ans;
-set<string> S;
-int main()
-{
-	cin>>n>>m;
-	FOR(i,0,n)
-	{
-		string t;
-		cin>>t;
-		S.insert(t);
+const int N = 600006, A = 313, MOD =  1110111110111;
+int n, m, base[N];
+set<int> S;
+string t;
+int get_hash(string &s){
+	int ret = 0;
+	FOR(i,0,s.size()){
+		ret += s[i]*base[i];
+		ret %= MOD;
 	}
-	FOR(i,0,m)
-	{
-		ans = 0;
-		string t;
+	return ret;
+}
+int32_t main(){
+	base[0] = 1;
+	FOR(i,1,N)
+		base[i] = base[i-1]*A % MOD;
+	cin>>n>>m;
+	while(n--){
 		cin>>t;
-		FOR(j,0,t.size())
-			FOR(l,0,3)
-				if(t[j] != 'a' + l)
-				{
-					char tmp = t[j];
-					t[j] = 'a' + l;
-					ans |= S.count(t);
-					t[j] = tmp;
-				}
-		cout<<(ans ? "YES":"NO")<<endl;
+		S.insert(get_hash(t));
+	}
+	while(m--){
+		bool ok = 0;
+		cin>>t;
+		int h = get_hash(t);
+		FOR(i,0,t.size())
+			FOR(j,'a','d')
+				if(j != t[i])
+					ok |= S.count((h+(j-t[i])*base[i]+4*MOD)%MOD);
+		cout<<(ok ? "YES":"NO")<<endl;
 	}
 }
